d6c2dd1bf498eff8dcf2174aa68ddd36898a913f
dinhanhduy1996 <96035586+dinhanhduy1996@users.noreply.github.com>
2022-10-03 23:55:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ const io = require('socket.io')(server, {
 });
 
 
-
+app.get('/', function(req, res){
+   res.send("Hello world!");
+});
+ 
 /*  */
     io.on('connection',(socket)=>{
         /* phấn của socket io, socket.on là nhận, io.mit là gởi */
